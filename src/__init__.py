b56d221973b20220ccdcfec85df4f85cf336a02d
mrohith29 <mariyalarohith29@gmail.com>
2024-02-06 18:27:46 +0530

    add setup and update __init__

--- src/__init__.py
+++ src/__init__.py
@@ -2,20 +2,30 @@ from argparse import Namespace, ArgumentParser
 import os
 import shutil
 
-parser = ArgumentParser(description='Copies the files from the sourse folder to the destination folder')
+def move():
+    parser = ArgumentParser(description='Copies the files from the source folder to the destination folder')
 
-parser.add_argument('source', type=str, help='Enter the source folder', const=os.listdir(), nargs='?')
+    parser.add_argument('source', type=str, help='Enter the source folder', action='store_true')
+    parser.add_argument('destination', type=str, help='Enter the destination folder', action='store_true')
 
-parser.add_argument('destination', type=str, help='Enter the destination folder', const=os.listdir(), nargs='?')
+    args: Namespace = parser.parse_args()
 
-args: Namespace = parser.parse_args()
+    if not os.path.isdir(args.source):
+        print(f"Source directory {args.source} does not exist.")
+        return
 
-for file in os.listdir(args.source):
-    file_path = os.path.join(args.source, file)
-    if os.path.isdir(file_path):
-        shutil.copytree(file_path, os.path.join(args.destination, file))
-    elif os.path.isfile(file_path):
-        shutil.copy(file_path, args.destination)
+    if not os.path.isdir(args.destination):
+        print(f"Destination directory {args.destination} does not exist.")
+        return
 
-print(f'Copying files from {args.source} to {args.destination}')
+    for file in os.listdir(args.source):
+        file_path = os.path.join(args.source, file)
+        if os.path.isdir(file_path):
+            shutil.copytree(file_path, os.path.join(args.destination, file))
+        elif os.path.isfile(file_path):
+            shutil.copy(file_path, args.destination)
 
+    print(f'Copying files from {args.source} to {args.destination}')
+
+if __name__ == "__main__":
+    move()
---
+++ src/setup.py
@@ -0,0 +1,35 @@
+import setuptools 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cpyfolder",
+    version="0.1.2",
+    description="Copy and paste your folders from the source to the destination folder",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mrohith29/copyfolder",
+    author="Mariyala Rohith",
+    author_email="mariyalarohith29@gmail.com",
+    license="MIT",
+    project_urls={
+        "Homepage": "https://github.com/mrohith29/copyfolder",
+        "Issues": "https://github.com/mrohith29/copyfolder/issues",
+    },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    include_package_data=True,
+    install_requires=["argparse", "shutin"],
+    entry_points={
+        "console_scripts": [
+            "move=move.__main__:move",
+        ]
+    },
+)
